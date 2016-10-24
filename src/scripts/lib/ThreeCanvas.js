47318ba7e92947b1268e4947a21363e55007afa7
yanwsh <yanwsh@gmail.com>
2016-10-23 23:05:18 -0400

    basic functionality.

--- src/scripts/lib/ThreeCanvas.js
+++ src/scripts/lib/ThreeCanvas.js
@@ -29,8 +29,11 @@ var ThreeCanvas = function (baseComponent, settings = {}){
             //define scene
             this.scene = new THREE.Scene();
             //define camera
-            this.camera = new THREE.PerspectiveCamera(options.initFov, this.width / this.height, 1, 2000);
-            this.camera.target = new THREE.Vector3( 0, 0, 0 );
+            this.cameraL = new THREE.PerspectiveCamera(options.initFov, this.width / this.height, 1, 2000);
+            this.cameraL.target = new THREE.Vector3( 0, 0, 0 );
+
+            this.cameraR = new THREE.PerspectiveCamera(options.initFov, this.width / this.height, 1, 2000);
+            this.cameraR.target = new THREE.Vector3( 1000, 0, 0 );
 
             //define render
             this.renderer = new THREE.WebGLRenderer();
@@ -63,8 +66,28 @@ var ThreeCanvas = function (baseComponent, settings = {}){
 
             var geometryL = new THREE.SphereGeometry(500, 60, 40);
             var geometryR = new THREE.SphereGeometry(500, 60, 40);
+            geometryR.position.set( 1000, 0, 0 );
 
+            var uvsL = geometryL.attributes.uv.array;
+            for ( var i = 0; i < uvsL.length / 2; i ++ ) {
+                uvsL[ i * 2 + 1 ] = uvsL[ i * 2 + 1 ] / 2;
+            }
 
+            var uvsR = geometryR.attributes.uv.array;
+            for ( var i = 0; i < uvsR.length / 2; i ++ ) {
+                uvsR[ i * 2 + 1 ] = uvsR[ i * 2 + 1 ] / 2;
+            }
+
+            geometryL.scale( - 1, 1, 1 );
+            geometryR.scale( - 1, 1, 1 );
+
+            this.meshL = new THREE.Mesh(geometryL,
+                new THREE.MeshBasicMaterial({ map: this.texture})
+            );
+
+            this.meshR = new THREE.Mesh(geometryR,
+                new THREE.MeshBasicMaterial({ map: this.texture})
+            );
         },
 
 
