3d9c27399a7ec3f715b45ad398808b405b3d8dcf
yanwsh <yanwsh@gmail.com>
2016-11-18 22:30:51 -0500

    update es6 syntax, convert es2015 syntax to vanilla javascript code to avoid compile error in ionic framework

--- dist/videojs-panorama.v5.js
+++ dist/videojs-panorama.v5.js
@@ -957,7 +957,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 /**
  * Created by yanwsh on 4/4/16.
